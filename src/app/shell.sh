7f9bb7c234f047c595e94df2914453283b01c90f
GabrielMM182 <gmm_morais@hotmail.com>
2021-09-29 14:40:07 -0300

    atualizacao de como instalar android

--- src/app/shell.sh
+++ src/app/shell.sh
@@ -1,1 +1,28 @@
-PAREI NA AULA 16 !!!
+- -PAREI NA AULA 16 !!!
+
+---------------- COMO INSTALAR CORRETAMENTE O ANDROID STUDIO -----------------
+https://www.youtube.com/watch?v=oKnG1VP43yY&list=LL&index=7
+
+$ JDK / SDK e GRADLE
+
+fazer donwload do arquivo jdk8
+* criar uma variavel de sistema chamada JAVA_HOME
+* Colocar o endereço de instalação do JDK em C:\ProgramFile\Java\jdk_____
+* Edite a variavel path das variaveis de sistema e adciione: %JAVA_HOME%\bin
+
+- - - BAIXAR ANDROID STUDIO - - - - 
+selecionar custom -> e trocar o caminho da pasta android para dentro do disco C
+configure SDK manager -> ( selecionar google USB driver / android sdk platform tools android sdk build tools) 
+* Crie uma nova variavel de sistema chamada: ANDROID_SDK_ROOT
+*coloque o endereço de instalação do SDK -> C:\ANDROID_SDK_ROOT
+
+*edite a variavel path das variaveis de sistema e adcione 
++ %ANDROID_SDK_ROOT%\tools
++ %ANDROID_SDK_ROOT%\platform-tools
++ %ANDROID_SDK_ROOT%\build-tools\_______ (sua versão instalada) 
+
+
+--- BAIXAR GRADLE ----
+ir em variaveis de ambiente / adicioonar novo path  / nono 
+
+C:\gradle\bin 
